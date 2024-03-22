732755be471271b5a2602f800ecef217e4bb5f4f
xxxxxii <1973329248@qq.com>
2024-03-22 10:51:18 +0800

    config

--- server/app/config/config.prd.js
+++ server/app/config/config.prd.js
@@ -5,7 +5,8 @@ const config = { ...configBase };
 // add mysql config
 config.database = {
   // client: "mysql2", 默认mysql
-  host: "154.221.19.228",
+  // host: "154.221.19.228",
+  host: "localhost",
   // port: "3306",默认3306
   user: "root",
   password: "Zyl123..",
